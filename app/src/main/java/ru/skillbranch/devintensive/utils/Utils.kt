5aedecb90750a5f4970f0ccdf3d6fed9aa44d001
neikist <neikist1992@gmail.com>
2019-07-02 08:08:33 +0300

    Написал Utils функции transliteration и toInitials

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -10,11 +10,59 @@ object Utils {
         }
     }
 
-    fun transliteration(payload: String, divider: String = " "): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun transliteration(payload: String, divider: String = " "): String = buildString {
+        payload.asSequence().forEach {
+            append(if (it == ' ') divider else it.transliterate())
+        }
     }
 
-    fun toInitials(firstName: String?, lastName: String?): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun toInitials(firstName: String?, lastName: String?): String = when {
+        (firstName == null || firstName.isBlank()) && (lastName == null || lastName.isBlank()) -> "null"
+        (firstName == null || firstName.isBlank()) && lastName != null -> lastName.trimStart().first().toString().toUpperCase()
+        firstName != null && (lastName == null || lastName.isBlank()) -> firstName.trimStart().first().toString().toUpperCase()
+        else -> (firstName!!.trimStart().first().toString() + lastName!!.trimStart().first()).toUpperCase()
+    }
+}
+
+private val chars = mapOf(
+        'а' to "a",
+        'б' to "b",
+        'в' to "v",
+        'г' to "g",
+        'д' to "d",
+        'е' to "e",
+        'ё' to "e",
+        'ж' to "zh",
+        'з' to "z",
+        'и' to "i",
+        'й' to "i",
+        'к' to "k",
+        'л' to "l",
+        'м' to "m",
+        'н' to "n",
+        'о' to "o",
+        'п' to "p",
+        'р' to "r",
+        'с' to "s",
+        'т' to "t",
+        'у' to "u",
+        'ф' to "f",
+        'х' to "h",
+        'ц' to "c",
+        'ч' to "ch",
+        'ш' to "sh",
+        'щ' to "sh'",
+        'ъ' to "",
+        'ы' to "i",
+        'ь' to "",
+        'э' to "e",
+        'ю' to "yu",
+        'я' to "ya")
+
+private fun Char.transliterate(): String {
+    return if (isUpperCase()) {
+        chars[this.toLowerCase()]?.toUpperCase() ?: this.toString()
+    } else {
+        chars[this] ?: this.toString()
     }
 }
