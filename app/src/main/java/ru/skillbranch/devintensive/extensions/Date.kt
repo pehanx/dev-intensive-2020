a3773a360dcbe3d4705955a4451ea22db26b5394
neikist <neikist1992@gmail.com>
2019-07-02 22:32:51 +0300

    Обновил humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -27,34 +27,42 @@ fun Date.add(value: Int, timeUnit: TimeUnits): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String = when {
-    this == date -> "только что"
-    this.time < date.time - 365 * DAY -> "более года назад"
-    this.time > date.time + 365 * DAY -> "более чем через год"
+    abs(this.time - date.time) <= SECOND && date > this -> "только что"
+    this.time < date.time - 360 * DAY -> "более года назад"
+    this.time > date.time + 360 * DAY -> "более чем через год"
     else -> {
+        val seconds = (abs(this.time - date.time) / SECOND).toInt()
         val template = if (this > date) "через %s" else "%s назад"
-        val (value, unit) = when (val diff = abs(this.time - date.time)) {
-            in 0 until MINUTE -> diff / SECOND to TimeUnits.SECOND
-            in MINUTE until HOUR -> diff / MINUTE to TimeUnits.MINUTE
-            in HOUR until DAY -> diff / HOUR to TimeUnits.HOUR
-            else -> diff / DAY to TimeUnits.DAY
-        }
-        template.format(pluralForm(value.toInt(), unit))
+        template.format(
+            when {
+                seconds in 1..45 -> "несколько секунд"
+                seconds in 45..75 -> "минуту"
+                seconds in 75..45 * 60 -> pluralForm(seconds / 60, TimeUnits.MINUTE)
+                seconds in 45 * 60..75 * 60 -> "час"
+                seconds in 75 * 60..22 * 3600 -> pluralForm(seconds / 3600, TimeUnits.HOUR)
+                seconds in 22 * 3600..26 * 3600 -> "день"
+                seconds in 26 * 3600..360 * 86400 -> pluralForm(seconds / 86400, TimeUnits.DAY)
+                else -> throw IllegalStateException()
+            }
+        )
     }
 
 }
 
 
 private fun pluralForm(value: Int, unit: TimeUnits): String {
+
+
     val form = when {
         value % 10 == 1 && value != 11 -> 0
         value % 10 in 2..4 -> 1
         else -> 2
     }
     return arrayListOf(
-            "%s секунда",
+        "%s секунду",
             "%s секунды",
             "%s секунд",
-            "%s минута",
+        "%s минуту",
             "%s минуты",
             "%s минут",
             "%s час",
