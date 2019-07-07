526a212fbbebb8b69551ee5b1fbd0b64c9c01afa
neikist <neikist1992@gmail.com>
2019-07-07 23:18:49 +0300

    Порешал задачку со строками

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,8 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(limit: Int = 16): String {
+	val trimmedString = this.trim()
+	if (trimmedString.length <= limit - 1) return trimmedString
+	val substr = trimmedString.substring(0 until limit).trim()
+	return "$substr..."
+}
