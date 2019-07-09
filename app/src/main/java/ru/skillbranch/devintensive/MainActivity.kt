c9b0d0b14819e0245d08b7ee6db1cd2731ddacff
neikist <neikist1992@gmail.com>
2019-07-09 21:20:44 +0300

    Миграция на androidx

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,6 +1,6 @@
 package ru.skillbranch.devintensive
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
