c8af2cd066bbd70354b526c417271657c66f7754
neikist <neikist1992@gmail.com>
2019-07-22 08:43:06 +0300

    fix цветов

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -29,10 +29,10 @@ class Bender(
 	}
 	
 	enum class Status(val color: Triple<Int, Int, Int>){
-		NORMAL(Triple(255,255,255)),
-		WARNING(Triple(255,120,0)),
-		DANGER(Triple(255,60,60)),
-		CRITICAL(Triple(255,255,0));
+		NORMAL(Triple(255, 255, 255)) ,
+		WARNING(Triple(255, 120, 0)),
+		DANGER(Triple(255, 60, 60)),
+		CRITICAL(Triple(255, 0, 0)) ;
 		
 		fun nextStatus(): Status {
 			return if (ordinal < values().lastIndex) {
