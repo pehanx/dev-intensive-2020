9b0331c36f5890e09885e7f8fd31f69290569934
neikist <neikist1992@gmail.com>
2019-07-25 21:47:46 +0300

    Начал 4

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package ru.skillbranch.devintensive.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.skillbranch.devintensive.R
+
+
+class ProfileActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
